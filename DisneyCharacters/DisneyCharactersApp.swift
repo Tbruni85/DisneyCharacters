0c41f249061f98d58976f9ba5d7ae86a50bdfa26
Tiziano Bruni <tiziano.bruni@gmail.com>
2024-04-24 10:31:24 +0100

    First commit

---
+++ DisneyCharacters/DisneyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyCharactersApp.swift
+//  DisneyCharacters
+//
+//  Created by Tiziano Bruni on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DisneyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
